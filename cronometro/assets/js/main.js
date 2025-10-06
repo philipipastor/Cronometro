b32bdc66df09d01d7bb4231ab067604348b2a6ab
Philipi Pastor <100240643+philipipastor@users.noreply.github.com>
2025-10-06 16:40:51 -0300

    Adicionando um escopo no meu código

--- cronometro/assets/js/main.js
+++ cronometro/assets/js/main.js
@@ -1,41 +1,46 @@
-const relogio = document.querySelector(".relogio")
-const button_iniciar = document.querySelector(".iniciar")
-const button_pausar = document.querySelector(".pausar")
-const button_zerar = document.querySelector(".zerar")
+function meu_escopo(){
 
-function mostra_hora(segundos) {
-    let data = new Date(segundos);
+    const relogio = document.querySelector(".relogio")
+    const button_iniciar = document.querySelector(".iniciar")
+    const button_pausar = document.querySelector(".pausar")
+    const button_zerar = document.querySelector(".zerar")
 
-    return data.toLocaleTimeString("pt-br", { hour12: false, timeZone: "GMT" });
+    function mostra_hora(segundos) {
+        let data = new Date(segundos);
+
+        return data.toLocaleTimeString("pt-br", { hour12: false, timeZone: "GMT" });
+    }
+
+    let segundos = 0;
+    let timer;
+
+    function iniciar() {
+        timer = setInterval(function () {
+            segundos++;
+            relogio.innerHTML = (mostra_hora(segundos * 1000));
+        }, 1000);
+    }
+
+
+    button_iniciar.addEventListener("click", function evento_botao1(e) {
+        e.preventDefault();
+        iniciar();
+        relogio.classList.remove("pausado");
+    });
+
+    button_pausar.addEventListener("click", function(e) {
+        e.preventDefault();
+        clearInterval(timer);
+        relogio.classList.add("pausado");
+    });
+
+    button_zerar.addEventListener("click", function(e) {
+        e.preventDefault();
+        segundos = 0
+        clearInterval(timer)
+        relogio.innerHTML = "00:00:00";
+        relogio.classList.remove("pausado");
+    });
 }
 
-let segundos = 0;
-let timer;
-
-function iniciar() {
-    timer = setInterval(function () {
-        segundos++;
-        relogio.innerHTML = (mostra_hora(segundos * 1000));
-    }, 1000);
-}
-
-
-button_iniciar.addEventListener("click", function evento_botao1(e) {
-    e.preventDefault();
-    iniciar();
-    relogio.classList.remove("pausado");
-});
-
-button_pausar.addEventListener("click", function(e) {
-    e.preventDefault();
-    clearInterval(timer);
-    relogio.classList.add("pausado");
-});
-
-button_zerar.addEventListener("click", function(e) {
-    e.preventDefault();
-    segundos = 0
-    clearInterval(timer)
-    relogio.innerHTML = "00:00:00";
-    relogio.classList.remove("pausado");
-});
+meu_escopo();
